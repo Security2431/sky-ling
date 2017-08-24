a54b22dbff90d1c0d1c8ed860215e6ec0cb41d44
Security2431 <Artem2431@gmail.com>
2017-08-24 23:42:56 +0300

    add tests, add link to logo, chenge to make post

--- test.php
+++ test.php
@@ -7,16 +7,56 @@ $admin_email  = trim($_POST["admin_email"]);
 
 $test_res = 0;
 
-if (trim($_POST['vopros-1']) === "b") $test_res++;
+if (trim($_POST['vopros-1']) === "a") $test_res++;
 if (trim($_POST['vopros-2']) === "b") $test_res++;
 if (trim($_POST['vopros-3']) === "b") $test_res++;
 if (trim($_POST['vopros-4']) === "b") $test_res++;
-if (trim($_POST['vopros-5']) === "a") $test_res++;
-if (trim($_POST['vopros-6']) === "a") $test_res++;
-if (trim($_POST['vopros-7']) === "a") $test_res++;
-if (trim($_POST['vopros-8']) === "c") $test_res++;
+if (trim($_POST['vopros-5']) === "c") $test_res++;
+if (trim($_POST['vopros-6']) === "b") $test_res++;
+if (trim($_POST['vopros-7']) === "c") $test_res++;
+if (trim($_POST['vopros-8']) === "a") $test_res++;
 if (trim($_POST['vopros-9']) === "a") $test_res++;
-if (trim($_POST['vopros-10']) === "d") $test_res++;
+if (trim($_POST['vopros-10']) === "c") $test_res++;
+if (trim($_POST['vopros-11']) === "b") $test_res++;
+if (trim($_POST['vopros-12']) === "b") $test_res++;
+if (trim($_POST['vopros-13']) === "b") $test_res++;
+if (trim($_POST['vopros-14']) === "a") $test_res++;
+if (trim($_POST['vopros-15']) === "a") $test_res++;
+if (trim($_POST['vopros-16']) === "c") $test_res++;
+if (trim($_POST['vopros-17']) === "c") $test_res++;
+if (trim($_POST['vopros-18']) === "a") $test_res++;
+if (trim($_POST['vopros-19']) === "b") $test_res++;
+if (trim($_POST['vopros-20']) === "c") $test_res++;
+if (trim($_POST['vopros-21']) === "c") $test_res++;
+if (trim($_POST['vopros-22']) === "b") $test_res++;
+if (trim($_POST['vopros-23']) === "b") $test_res++;
+if (trim($_POST['vopros-24']) === "a") $test_res++;
+if (trim($_POST['vopros-25']) === "b") $test_res++;
+if (trim($_POST['vopros-26']) === "b") $test_res++;
+if (trim($_POST['vopros-27']) === "a") $test_res++;
+if (trim($_POST['vopros-28']) === "a") $test_res++;
+if (trim($_POST['vopros-29']) === "b") $test_res++;
+if (trim($_POST['vopros-30']) === "b") $test_res++;
+if (trim($_POST['vopros-31']) === "b") $test_res++;
+if (trim($_POST['vopros-32']) === "a") $test_res++;
+if (trim($_POST['vopros-33']) === "c") $test_res++;
+if (trim($_POST['vopros-34']) === "a") $test_res++;
+if (trim($_POST['vopros-35']) === "a") $test_res++;
+if (trim($_POST['vopros-36']) === "b") $test_res++;
+if (trim($_POST['vopros-37']) === "a") $test_res++;
+if (trim($_POST['vopros-38']) === "b") $test_res++;
+if (trim($_POST['vopros-39']) === "a") $test_res++;
+if (trim($_POST['vopros-40']) === "b") $test_res++;
+if (trim($_POST['vopros-41']) === "b") $test_res++;
+if (trim($_POST['vopros-42']) === "a") $test_res++;
+if (trim($_POST['vopros-43']) === "b") $test_res++;
+if (trim($_POST['vopros-44']) === "a") $test_res++;
+if (trim($_POST['vopros-45']) === "b") $test_res++;
+if (trim($_POST['vopros-46']) === "a") $test_res++;
+if (trim($_POST['vopros-47']) === "b") $test_res++;
+if (trim($_POST['vopros-48']) === "a") $test_res++;
+if (trim($_POST['vopros-49']) === "c") $test_res++;
+if (trim($_POST['vopros-50']) === "c") $test_res++;
 
 $mail->setFrom('no-reply@sky-ling.com', 'Sky-ling. Школа английского');
 $mail->AddAddress($admin_email);     // Add a recipient
@@ -40,7 +80,7 @@ $mail->Body    = '<p style="color: #000000;">Здравствуйте, спас�
 <br>
 https://goo.gl/RMzjaQ - 6 советов по изучению английских слов
 <br>
-https://goo.gl/oqjSJB - DO vs Make
+https://goo.gl/6DqZzc - DO vs Make
 <br>
 https://goo.gl/AZb6bs - Чтение и выполнение задания
 <br>
